92cc25ade5e4a84a3249c0fcbea5eb0e8f9f5c15
Alain Guimberteau <alain.guimberteau@laposte.net>
2019-10-05 14:54:01 +0200

    Avant dernier module

--- Program.cs
+++ Program.cs
@@ -1,101 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace HelloWorld
 {
-    public abstract class Tools
-    {
-        public static double Age(DateTime Birth)
-        {
-            return ((DateTime.Now - Birth).Days / 365.25);
-        }
-    }
-    public class Person
-    {
-        public string Nom { get; set; }
-        public string Prénom { get; set; }
-        public DateTime Birth { get; set; }
-        public Person(string nom, string prenom, DateTime birth)
-        {
-            Nom = nom;
-            Prénom = prenom;
-            Birth = birth;
-        }
-    }
-    public class Phoneux : Person
-    {
-        public string Numéro { get; set; }
-        public Phoneux(string nom, string prenom, DateTime birth, string numero) : base(nom, prenom, birth)
-        {
-            Numéro = numero;
-        }
-    }
-
-    public delegate void Del(string message);
-
     class Program
     {
-        // Create a method for a delegate.
-        public static void DelegateMethod(string message)
-        {
-            System.Console.WriteLine(message);
-        }
         static void Main(string[] args)
         {
-
-            #region Classe
-            string Nom = Console.ReadLine();
-            string Prénom = Console.ReadLine();
-            DateTime Birth = Convert.ToDateTime(Console.ReadLine());
-
-            Person p = new Person(Nom, Prénom, Birth);
-
-            Phoneux Ph = new Phoneux(Nom, Prénom, Birth, "0618041027");
-
-            Console.WriteLine($"Call {Ph.Prénom} at {Ph.Numéro}...");
-            Console.WriteLine("Age = " + Tools.Age(Ph.Birth).ToString());
-            Console.WriteLine($"Hello {p.Prénom} !!!");
-
+            Console.WriteLine("Hello World");
             Console.ReadLine();
-            #endregion Classe
-
-            #region Tableau
-            int[] tints = { 3, 2, 1 };
-            foreach (var tint in tints)
-            {
-                Console.WriteLine(tint);
-            }
-            Console.ReadLine();
-            Array.Sort(tints);
-            foreach (var tint in tints)
-            {
-                Console.WriteLine(tint);
-            }
-            Console.ReadLine();
-            #endregion Tableau
-
-            #region Liste
-            List<int> li = new List<int>();
-            li.Add(3);
-            li.Add(2);
-            li.Add(1);
-            li.Sort();
-            foreach (var tint in li)
-            {
-                Console.WriteLine(tint);
-            }
-            Console.ReadLine();
-            #endregion Liste
-
-            #region Delegate
-            // Instantiate the delegate.
-            Del handler = DelegateMethod;
-
-            // Call the delegate.
-            handler("Hello World");
-            Console.ReadLine();
-            #endregion Delegate
-
         }
     }
 }
