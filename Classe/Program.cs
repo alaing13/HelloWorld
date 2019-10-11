e2c4ae3229b9ff93d23587086df7b90b7d666ca6
Alain Guimberteau <alain.guimberteau@laposte.net>
2019-10-11 11:33:14 +0200

    3 projets de plus

---
+++ Classe/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Classe
+{
+    public abstract class Tools
+    {
+        public static double Age(DateTime Birth)
+        {
+            return ((DateTime.Now - Birth).Days / 365.25);
+        }
+    }
+    public class Person
+    {
+        public string Nom { get; set; }
+        public string Prénom { get; set; }
+        public DateTime Birth { get; set; }
+        public Person(string nom, string prenom, DateTime birth)
+        {
+            Nom = nom;
+            Prénom = prenom;
+            Birth = birth;
+        }
+    }
+    public class Phoneux : Person
+    {
+        public string Numéro { get; set; }
+        public Phoneux(string nom, string prenom, DateTime birth, string numero) : base(nom, prenom, birth)
+        {
+            Numéro = numero;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Entrez votre nom :");
+            string Nom = Console.ReadLine();
+            Console.WriteLine("Entrez votre prénom :");
+            string Prénom = Console.ReadLine();
+            DateTime Birth = GetBirth();
+
+            Person p = new Person(Nom, Prénom, Birth);
+
+            Phoneux Ph = new Phoneux(Nom, Prénom, Birth, "0618041027");
+
+            Console.WriteLine($"Call {Ph.Prénom} at {Ph.Numéro}...");
+            Console.WriteLine($"Age = {Tools.Age(Ph.Birth)} ans...");
+            Console.WriteLine($"Hello {p.Prénom} !!!");
+
+            Console.ReadLine();
+
+        }
+        private static DateTime GetBirth()
+        {
+            Console.WriteLine("Entrez votre date de naissance :");
+            DateTime Birth = Convert.ToDateTime(Console.ReadLine());
+            return Birth;
+        }
+    }
+}
---
+++ Liste/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Liste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Méthode ancienne
+            //List<int> li = new List<int>();
+            //li.Add(3);
+            //li.Add(2);
+            //li.Add(1);
+            // Méthode nouvelle
+            List<int> li = new List<int>
+            {
+                3,
+                2,
+                1
+            };
+            li.Sort();
+            foreach (var tint in li)
+            {
+                Console.WriteLine(tint);
+            }
+            Console.ReadLine();
+        }
+    }
+}
